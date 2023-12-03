4ee3ae09d5d9f9d43fbe7f97385f97adf26674a6
jawney-anshita <jawney.anshita@gmail.com>
2023-12-03 13:22:54 +0530

    added static params

--- app/api/products/[productid]/route.js
+++ app/api/products/[productid]/route.js
@@ -3,6 +3,22 @@ import mongoose from "mongoose";
 import { connectionSrt} from "../../../../src/lib/db"; 
 import { Product } from "../../../../src/lib/model/product";
 
+// Return a list of `params` to populate the [slug] dynamic segment
+export async function generateStaticParams() {
+    const posts = await fetch('').then((res) => res.json())
+   
+    return posts.map((post) => ({
+      slug: post.slug,
+    }))
+  }
+   
+  // Multiple versions of this page will be statically generated
+  // using the `params` returned by `generateStaticParams`
+  export default function Page({ params }) {
+    const { slug } = params
+    // ...
+  } 
+
 export async function PUT(request, content){
    
     const productId = content.params.productid;
