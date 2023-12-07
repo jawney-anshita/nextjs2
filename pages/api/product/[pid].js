36f7a914cb732b7b819ba30e87070b80f550cde2
jawney-anshita <jawney.anshita@gmail.com>
2023-12-07 20:16:42 +0530

    remove product api

--- pages/api/product/[pid].js
+++
@@ -1,8 +0,0 @@
-import { Product } from "../../../src/lib/model/product";
-
-export default async (req,res)=>{
-    const {pid} = req.query
-        const product = await Product.findOne({_id:pid})
-
-    res.status(200).json(product)
-}
