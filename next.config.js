672527a54bfc6859015d839d20116885231cd37c
jawney-anshita <jawney.anshita@gmail.com>
2023-12-07 16:22:06 +0530

    config file changes

--- next.config.js
+++ next.config.js
@@ -1,20 +1,9 @@
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-  typescript:{
-    ignoreBuildErrors:true,
-  },
-    // output: 'export',
-   
-    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-    
-   
-    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-    // skipTrailingSlashRedirect: true,
-   
-    // Optional: Change the output directory `out` -> `dist`
-    // distDir: 'dist',
+module.exports = async (phase, { defaultConfig }) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    /* config options here */
   }
-   
-  module.exports = nextConfig
+  return nextConfig
+}
